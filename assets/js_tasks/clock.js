9ef40f98438ad6332ed323f820890fe64c67ed44
Absorbo-web <62425210+Absorbo-web@users.noreply.github.com>
2020-04-01 19:25:10 +0300

    Проверка формы
    Добавил проверку формы, задание по js почти готово

--- assets/js_tasks/clock.js
+++ assets/js_tasks/clock.js
@@ -4,22 +4,22 @@ var clock = new Vue({
     time: '',  
     date: ''  
     }  
-    });  
+});  
   
-    var week = ['ВС', 'ПОН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СУБ'];  
-    var timerID = setInterval(updateTime, 1000);  
-    updateTime();  
+var week = ['ВС', 'ПОН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СУБ'];  
+var timerID = setInterval(updateTime, 1000);  
+updateTime();  
   
-    function updateTime() {  
+function updateTime() {  
     var cd = new Date();  
     clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);  
     clock.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getDate(), 2) + '-' + zeroPadding(cd.getMonth() + 1, 2) + ' ' + week[cd.getDay()];  
-    };  
+};  
   
-    function zeroPadding(num, digit) {  
+function zeroPadding(num, digit) {  
     var zero = '';  
     for (var i = 0; i < digit; i++) {  
     zero += '0';  
     }  
     return (zero + num).slice(-digit);  
-    }  
+}  
